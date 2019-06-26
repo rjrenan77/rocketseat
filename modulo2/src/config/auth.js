e5505d968dab221d80b9ac4bbcf4f1167a328229
renan <renansoares.cro-rj.org.br>
2019-06-26 14:44:59 -0300

    modulo 2 nodejs

---
+++ modulo2/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '45910ee37483dff6e6a95f31003e0fd5',
+  expiresIn: '7d',
+};
---
+++ modulo2/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '147852369',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
